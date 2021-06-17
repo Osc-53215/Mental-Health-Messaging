cf3f3bf353a715777f33b0876fb2119f0f29f662
Osc-53215 <80727193+Osc-53215@users.noreply.github.com>
2021-06-17 18:10:59 -0500

    installed router and imported everything i needed for it

--- mh-messaging/src/App.js
+++ mh-messaging/src/App.js
@@ -1,10 +1,26 @@
 import React from 'react';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 function App() {
   return (
     <div className="app">
-      <h1>Testing</h1>
+
+      <Router>
+      <>
+        <Switch>
+          <Route path="/" exact>
+
+            <h1>Testing thee homepage</h1>
+          </Route>
+        </Switch>
+      </>
+    </Router>
     </div>
   );
 }
